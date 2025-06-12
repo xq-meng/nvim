968345b1365add1017fb41963ece8e17e03b3803
xqmeng <xiangqiao.meng@qq.com>
2025-06-12 19:56:06 +0800

    feat: remove extra dependencies

--- lua/plugins/hello-nvim.lua
+++ lua/plugins/hello-nvim.lua
@@ -1,8 +1,5 @@
 return {
     "xq-meng/hello-nvim",
-    dependencies = {
-        'echasnovski/mini.icons',
-    },
     config = function()
         require("hello").setup(require("hello.themes.dashboard").opts)
     end
